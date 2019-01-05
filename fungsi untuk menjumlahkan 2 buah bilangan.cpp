59ae13e819a08cf96a3b98d6169ad34d580b063b
VIKTORKEVIN <46395918+VIKTORKEVIN@users.noreply.github.com>
2019-01-05 19:37:06 +0700

    Add files via upload

---
+++ fungsi untuk menjumlahkan 2 buah bilangan.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+int main ()
+{
+    cout<<"VIKTOR CEVIN ANJOLA"<<endl;
+    cout<<"311810020"<<endl;
+    cout<<"TI.18 D7"<<endl;
+    cout<<"=============================================="<<endl;
+    cout<<"Program untuk menjumlahkan 2 buah bilangan"<<endl;
+    cout<<"=============================================="<<endl;
+
+    int bilangan1, bilangan2;
+    string operasi;
+    cout<<"masukkan angka pertama = ";
+    cin>>bilangan1;
+    cout<<"masukkan bilangan kedua = ";
+    cin>>bilangan2;
+    cout<<"masukkan operasi bilangan tersebut = ";
+    cin>>operasi;
+
+    if (operasi == "+")
+    {
+        cout<<"Hasilnya ="<<(bilangan1)+(bilangan2)<<endl;
+    }
+    if (operasi == "-")
+    {
+        cout<<"Hasilnya ="<<(bilangan1)-(bilangan2)<<endl;
+    }
+    if (operasi == "*")
+    {
+        cout<<"Hasilnya ="<<(bilangan1)*(bilangan2)<<endl;
+    }
+    if (operasi == "/")
+    {
+        cout<<"Hasilnya ="<<(bilangan1)/(bilangan2)<<endl;
+    }
+    return 0;
+}
